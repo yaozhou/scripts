aba2a7a746f7ab9712f61c300a69e36ef5e3481e
Yao <yaozhou.wuhu@gmail.com>
2015-11-22 14:44:50 +0800

    modify

--- wiki_backup.sh
+++ wiki_backup.sh
@@ -7,7 +7,7 @@ back_dir_client="/home/yao/Dropbox/private/document/wiki_backup/${day}.sql.gz"
 
 cmd="mysqldump -h localhost -u root -pna my_wiki | gzip > ${back_file_server}"
 
-echo "正在生成备份..."
+logger "正在生成备份..."
 ssh na@linode "$cmd"
-echo "正在拉取备份文件..."
+logger "正在拉取备份文件..."
 scp na@linode:${back_file_server} ${back_dir_client}
