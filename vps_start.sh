d7ce262ac6d61c7d319917ffb6818495d1636696
Yao Ali <yaozhou.wuhu@gmail.com>
2017-11-07 17:40:26 +0800

    add vps script

--- vps_start.sh
+++ vps_start.sh
@@ -2,18 +2,27 @@
 
 #/home/yao/develop/todolist2/run.sh
 cd /home/yao/web_static && nohup http-server -p 8000 > /tmp/t1 &
-cd /home/yao/blog && nohup http-server -p 8001 > /tmp/t2 &
+#cd /home/yao/blog && nohup http-server -p 8001 > /tmp/t2 &
 
 /home/yao/develop/scripts/ss_local.sh
+
+cd /home/yao/develop/scripts/proxy
 sudo nohup node /home/yao/develop/scripts/proxy/proxy.js > /tmp/proxy.out &
+
 cd /home/yao/develop/logic-test
 nohup node /home/yao/develop/logic-test/main.js &
+
+cd /home/yao/develop/ear-travel
 nohup node /home/yao/develop/ear-travel/main.js &
-/home/yao/develop/NodeBB/nodebb start
+#/home/yao/develop/NodeBB/nodebb start
 
 cd /home/yao/develop/ele/heatmap/out
 nohup http-server -p 8889 &
 
+nohup superset runserver &
 
+#sudo service mariadb start
 
+nohup redis-server &
 
+sudo mount -t nfs -o vers=4.0  0e42648e07-esb49.cn-hangzhou.nas.aliyuncs.com:/ /nfs
